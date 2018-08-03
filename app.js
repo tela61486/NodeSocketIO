1d19b9dce95f13cb65a262f4b3b3d0be71fc564b
brandon thomas <Tela@brandons-MacBook-Air.local>
2018-08-03 17:57:01 -0500

    added comments

--- app.js
+++ app.js
@@ -35,8 +35,8 @@ var server = http.createServer(function(req,res){
 // WebSockets thanks to socket.io
 var io = require("socket.io").listen(server);
 
-
-// When a client connects, we note it in the console
+// WSocketIO
+/// When a client connects, we note it in the console
 //io.sockets.on('connection', function(socket){
 
 //    console.log('A client is connected!');
